8eafd779e2e2d911f556dfca807b616cc8904cdb
Philippe M. Chiasson <gozer@mozillamessaging.com>
2008-09-29 17:32:54 -0400

    Backed out changeset 2137e1924abb

---
+++ mail/components/search/nsWinSearchIntegration.js
@@ -0,0 +1,370 @@
+# -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is spotlight integration code.
+#
+# The Initial Developer of the Original Code is
+# David Bienvenu <bienvenu@mozilla.com>
+# Portions created by the Initial Developer are Copyright (C) 2007
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#  Siddharth Agarwal <sid1337@gmail.com>
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
+#include content/searchCommon.js
+
+const MSG_DB_LARGE_COMMIT = 1;
+const MSG_FLAG_ATTACHMENT = 0x10000000;
+const CRLF="\r\n";
+
+// The property of the header that's used to check if a message is indexed
+const gHdrIndexedProperty = "wds_indexed";
+
+// The file extension that is used for support files of this component
+const gFileExt = ".wdseml";
+
+// The pref base
+const gPrefBase = "mail.winsearch";
+
+var gWinSearchHelper;
+
+var gFoldersInCrawlScope;
+
+var gRegKeysPresent;
+
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+function InitWinSearchIntegration()
+{
+  // We're currently only enabled on Vista and above
+  var sysInfo = Cc["@mozilla.org/system-info;1"].getService(Ci.nsIPropertyBag2);
+  var windowsVersion = sysInfo.getProperty("version");
+  if (parseFloat(windowsVersion) < 6)
+  {
+    SIDump("Windows version " + windowsVersion + " < 6.0\n");
+    return;
+  }
+
+  // enabled === undefined means that the first run hasn't occurred yet (pref isn't present).
+  // false or true means that the first run has occurred, and the user has selected
+  // the corresponding decision.
+  var enabled;
+  try {
+    enabled = gPrefBranch.getBoolPref(gPrefBase + ".enable");
+    gLastFolderIndexedUri = gPrefBranch.getCharPref(gPrefBase + ".lastFolderIndexedUri");
+  } catch (ex) {}
+
+  gWinSearchHelper = Cc["@mozilla.org/mail/windows-search-helper;1"].getService(Ci.nsIMailWinSearchHelper);
+  var serviceRunning = false;
+  try
+  {
+    serviceRunning = gWinSearchHelper.serviceRunning;
+  }
+  catch (e) {}
+  // If the service isn't running, then we should stay in backoff mode
+  if (!serviceRunning)
+  {
+    SIDump("Windows Search service not running\n");
+    InitSupportIntegration(false);
+    return;
+  }
+
+  gFoldersInCrawlScope = gWinSearchHelper.foldersInCrawlScope;
+  gRegKeysPresent = CheckRegistryKeys();
+
+  if (enabled === undefined)
+    // First run has to be handled after the main mail window is open
+    return true;
+
+  if (enabled)
+    SIDump("Initializing Windows Search integration\n");
+  InitSupportIntegration(enabled);
+}
+
+// Handles first run, once the main mail window has popped up.
+function WinSearchFirstRun(window)
+{
+  // If any of the two are not present, we need to elevate.
+  var needsElevation = !gFoldersInCrawlScope || !gRegKeysPresent;
+  var params = {in: {showUAC: needsElevation}};
+  var scope = this;
+
+  params.callback = function(enable)
+  {
+    CheckRegistryKeys();
+    if (enable && needsElevation)
+    {
+      try { scope.gWinSearchHelper.runSetup(true); }
+      catch (e) { enable = false; }
+    }
+    if (enable)
+    {
+      if (!scope.gWinSearchHelper.isFileAssociationSet)
+      {
+        try { scope.gWinSearchHelper.setFileAssociation(); }
+        catch (e) { SIDump("File association not set\n"); }
+      }
+      // Also set the FANCI bit to 0 for the profile directory
+      scope.gWinSearchHelper.setFANCIBit(Cc["@mozilla.org/file/directory_service;1"]
+                                         .getService(Ci.nsIProperties).get("ProfD", Ci.nsIFile),
+                                         false, true);
+    }
+    scope.gPrefBranch.setBoolPref(gPrefBase + ".enable", enable);
+    scope.InitSupportIntegration(enable);
+  }
+
+  window.openDialog("chrome://messenger/content/search/searchIntegrationDialog.xul", "",
+                    "chrome, dialog, resizable=no, centerscreen", params).focus();
+}
+
+const gRegKeys =
+[
+  // This is the property handler
+  {
+    root: Ci.nsIWindowsRegKey.ROOT_KEY_LOCAL_MACHINE,
+    key: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PropertySystem\\PropertyHandlers\\.wdseml",
+    name: "",
+    value: "{5FA29220-36A1-40f9-89C6-F4B384B7642E}"
+  },
+  // These two are the association with the MIME IFilter
+  {
+    root: Ci.nsIWindowsRegKey.ROOT_KEY_CLASSES_ROOT,
+    key: ".wdseml",
+    name: "Content Type",
+    value: "message/rfc822"
+  },
+  {
+    root: Ci.nsIWindowsRegKey.ROOT_KEY_CLASSES_ROOT,
+    key: ".wdseml\\PersistentHandler",
+    name: "",
+    value: "{5645c8c4-e277-11cf-8fda-00aa00a14f93}"
+  },
+  // This is the association with the Windows mail preview handler
+  {
+    root: Ci.nsIWindowsRegKey.ROOT_KEY_CLASSES_ROOT,
+    key: ".wdseml\\shellex\\{8895B1C6-B41F-4C1C-A562-0D564250836F}",
+    name: "",
+    value: "{b9815375-5d7f-4ce2-9245-c9d4da436930}"
+  },
+  // This is the association made to display results under email
+  {
+    root: Ci.nsIWindowsRegKey.ROOT_KEY_LOCAL_MACHINE,
+    key: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\explorer\\KindMap",
+    name: ".wdseml",
+    value: "email;communication"
+  }
+];
+
+// Check whether the required registry keys exist
+function CheckRegistryKeys()
+{
+  for (var i = 0; i < gRegKeys.length; i++)
+  {
+    var regKey = Cc["@mozilla.org/windows-registry-key;1"].createInstance(Ci.nsIWindowsRegKey);
+    try {
+      regKey.open(gRegKeys[i].root, gRegKeys[i].key, regKey.ACCESS_READ);
+    }
+    catch (e) { return false; }
+    var valuePresent = regKey.hasValue(gRegKeys[i].name) &&
+                        (regKey.readStringValue(gRegKeys[i].name) == gRegKeys[i].value);
+    regKey.close();
+    if (!valuePresent)
+      return false;
+  }
+  return true;
+}
+
+// The stream listener to read messages
+var gStreamListener = {
+_buffer: "",
+outputFile: null,
+outputStream: null,
+unicodeConverter: null,
+// subject: null,
+message: "",
+msgHdr: null,
+mimeHdrObj: null,
+mimeHdrParamObj: null,
+
+onDoneStreamingCurMessage: function(successful)
+{
+  if (this.outputStream)
+    this.outputStream.close();
+  if (!successful && this.msgHdr)
+  {
+    var file = GetSupportFileForMsgHdr(this.msgHdr);
+    if (file && file.exists())
+      file.remove(false);
+  }
+  // should we try to delete the file on disk in case not successful?
+  gMsgHdrsToIndex.shift();
+
+  if (gMsgHdrsToIndex.length > 0)
+    GenerateSupportFile(gMsgHdrsToIndex[0]);
+},
+
+QueryInterface: function(aIId, instance) {
+  if (aIId.equals(Ci.nsIStreamListener) || aIId.equals(Ci.nsISupports))
+    return this;
+
+  Components.returnCode = Components.results.NS_ERROR_NO_INTERFACE;
+  return null;
+},
+
+onStartRequest: function(request, context) {
+  try
+  {
+    var outputFileStream =  Cc["@mozilla.org/network/file-output-stream;1"]
+      .createInstance(Ci.nsIFileOutputStream);
+    outputFileStream.init(this.outputFile, -1, -1, 0);
+    this.outputStream = Cc["@mozilla.org/intl/converter-output-stream;1"]
+      .createInstance(Ci.nsIConverterOutputStream);
+    this.outputStream.init(outputFileStream, "UTF-8", 0, 0x0000);
+  }
+  catch (ex)
+  {
+    onDoneStreamingCurMessage(false);
+  }
+},
+
+onStopRequest: function(request, context, status, errorMsg) {
+  try
+  {
+    // XXX Once the JS emitter gets checked in, this code should probably be
+    // switched over to use that
+    // Decode using getMsgTextFromStream
+    var stringStream = Cc["@mozilla.org/io/string-input-stream;1"].
+      createInstance(Ci.nsIStringInputStream);
+    stringStream.setData(this.message, this.message.length);
+    var contentType = {};
+    var text = this.msgHdr.folder.getMsgTextFromStream(stringStream, this.msgHdr.charset,
+                                                       65536, 50000, false, false, contentType);
+
+    this.outputStream.writeString("From: " + this.msgHdr.author + CRLF);
+    this.outputStream.writeString("To: " + this.msgHdr.recipients + CRLF);
+    this.outputStream.writeString("CC: " + this.msgHdr.ccList + CRLF);
+    this.outputStream.writeString("Subject: " + this.msgHdr.subject + CRLF);
+    this.outputStream.writeString("Date: " + new Date(this.msgHdr.date / 1000).toUTCString() + CRLF);
+    this.outputStream.writeString("Content-Type: " + contentType.value + "; charset=utf-8" + CRLF + CRLF);
+
+    this.outputStream.writeString(text + CRLF + CRLF);
+
+    this.msgHdr.setUint32Property(gHdrIndexedProperty, 1);
+    var msgDB = this.msgHdr.folder.getMsgDatabase(null);
+    msgDB.Commit(MSG_DB_LARGE_COMMIT);
+
+    this.message = "";
+    SIDump("Successfully written file\n");
+  }
+  catch (ex)
+  {
+    SIDump(ex);
+    this.onDoneStreamingCurMessage(false);
+    return;
+  }
+  this.onDoneStreamingCurMessage(true);
+},
+
+onDataAvailable: function(request, context, inputStream, offset, count) {
+  try
+  {
+    var inStream = Cc["@mozilla.org/scriptableinputstream;1"].createInstance(Ci.nsIScriptableInputStream);
+    inStream.init(inputStream);
+
+    // It is necessary to read in data from the input stream
+    var inData = inStream.read(count);
+
+    // If we've already reached the attachments, safely ignore.
+    if (this.filteredAttachments)
+      return 0;
+
+    // Also ignore stuff after the first 50K or so
+    if (this.message && this.message.length > 50000)
+      return 0;
+    var inStream = Cc["@mozilla.org/scriptableinputstream;1"].
+      createInstance(Ci.nsIScriptableInputStream);
+
+    inStream.init(inputStream);
+
+    this.message += inData;
+    return 0;
+  }
+  catch (ex)
+  {
+    SIDump(ex);
+    onDoneStreamingCurMessage(false);
+  }
+}
+};
+
+/* XPCOM boilerplate code */
+function WinSearchIntegration() { }
+WinSearchIntegration.prototype = {
+  classDescription: "Windows Search Integration",
+  classID: Components.ID("{451a70f0-1b4f-11dd-bd0b-0800200c9a66}"),
+  contractID: "@mozilla.org/windows-search-integration;1",
+
+  _xpcom_categories: [{
+    category: "app-startup",
+    service: true
+  }],
+
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver, Ci.nsISupports]),
+
+  observe : function(aSubject, aTopic, aData)
+  {
+    var obsSvc = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
+    switch(aTopic)
+    {
+    case "app-startup":
+      obsSvc.addObserver(this, "profile-after-change", false);
+      break;
+    case "profile-after-change":
+      try
+      {
+        if (InitWinSearchIntegration())
+          obsSvc.addObserver(this, "mail-startup-done", false);
+      }
+      catch(err) { SIDump("Could not initialize winsearch component"); }
+      break;
+    case "mail-startup-done":
+      aSubject.QueryInterface(Ci.nsIDOMWindowInternal);
+      obsSvc.removeObserver(this, "mail-startup-done");
+      try { WinSearchFirstRun(aSubject); }
+      catch(err) { SIDump("First run unsuccessful\n"); }
+      break;
+    default:
+      throw Components.Exception("Unknown topic: " + aTopic);
+    }
+  }
+};
+var components = [WinSearchIntegration];
+function NSGetModule(aCompMgr, aFileSpec)
+{
+  return XPCOMUtils.generateModule(components);
+}
---
+++ mailnews/base/prefs/resources/content/accountUtils.js
@@ -0,0 +1,297 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 1998
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Alec Flett <alecf@netscape.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+var accountManagerContractID   = "@mozilla.org/messenger/account-manager;1";
+var gAnyValidIdentity = false; //If there are no valid identities for any account
+// returns the first account with an invalid server or identity
+
+var gNewAccountToLoad = null;   // used to load new messages if we come from the mail3pane
+
+function getInvalidAccounts(accounts)
+{
+    var numAccounts = accounts.Count();
+    var invalidAccounts = new Array;
+    var numIdentities = 0;
+    for (var i=0; i<numAccounts; i++) {
+        var account = accounts.QueryElementAt(i, Components.interfaces.nsIMsgAccount);
+        try {
+            if (!account.incomingServer.valid) {
+                invalidAccounts[invalidAccounts.length] = account;
+                // skip to the next account
+                continue;
+            }
+        } catch (ex) {
+            // this account is busted, just keep going
+            continue;
+        }
+
+        var identities = account.identities;
+        numIdentities = identities.Count();
+
+        for (var j=0; j<numIdentities; j++) {
+            var identity = identities.QueryElementAt(j, Components.interfaces.nsIMsgIdentity);
+            if (identity.valid) {
+              gAnyValidIdentity = true;
+            }
+            else {
+              invalidAccounts[invalidAccounts.length] = account;
+            }
+        }
+    }
+    return invalidAccounts;
+}
+
+function showMailIntegrationDialog() {
+  const nsIShellService = Components.interfaces.nsIShellService;
+
+  try {
+    var shellService = Components.classes["@mozilla.org/suite/shell-service;1"]
+                                 .getService(nsIShellService);
+    var accountManager = Components.classes[accountManagerContractID].getService(Components.interfaces.nsIMsgAccountManager);
+    var defaultAccount = accountManager.defaultAccount;
+    var appTypesCheck = shellService.shouldBeDefaultClientFor &
+                        (nsIShellService.MAIL | nsIShellService.NEWS);
+
+    // show the default client dialog only if we have at least one account,
+    // if we should check for the default client, and we want to check if we are 
+    // the default for mail/news and are not the default client for mail/news
+    if (appTypesCheck && shellService.shouldCheckDefaultClient &&
+        !shellService.isDefaultClient(true, appTypesCheck))
+        window.openDialog("chrome://communicator/content/defaultClientDialog.xul",
+                        "DefaultClient", "modal,centerscreen,chrome,resizable=no");
+  } catch (ex) {}
+}
+
+function verifyAccounts(wizardCallback) 
+{
+	var openWizard = false;
+  var prefillAccount;
+	var state=true;
+	var ret = true;
+    
+    try {
+        var am = Components.classes[accountManagerContractID].getService(Components.interfaces.nsIMsgAccountManager);
+
+        // migrate quoting preferences from global to per account. This function returns
+        // true if it had to migrate, which we will use to mean this is a just migrated
+        // or new profile
+        var newProfile = migrateGlobalQuotingPrefs(am.allIdentities);
+
+        var accounts = am.accounts;
+
+        // as long as we have some accounts, we're fine.
+        var accountCount = accounts.Count();
+        var invalidAccounts = getInvalidAccounts(accounts);
+        if (invalidAccounts.length > 0) {
+            prefillAccount = invalidAccounts[0];
+        } else {
+        }
+
+        // if there are no accounts, or all accounts are "invalid"
+        // then kick off the account migration. Or if this is a new (to Mozilla) profile.
+        // MCD can set up accounts without the profile being used yet
+        if (newProfile) {
+          // check if MCD is configured. If not, say this is not a new profile
+          // so that we don't accidentally remigrate non MCD profiles.
+          var adminUrl;
+          var pref = Components.classes["@mozilla.org/preferences-service;1"]
+                                 .getService(Components.interfaces.nsIPrefBranch);
+          try {
+            adminUrl = pref.getCharPref("autoadmin.global_config_url");
+          }
+          catch (ex) {}
+          if (!adminUrl)
+            newProfile = false;
+        }
+        if ((newProfile  && !accountCount) || accountCount == invalidAccounts.length)
+          openWizard = true;
+
+        //We are doing openWizard if  MessengerMigration returns some kind of error
+        //(including those cases where there is nothing to migrate).
+        //prefillAccount is valid, if there is an invalid account already 
+        //gAnyValidIdentity is true when you've got at least one *valid* identity,
+        //Since local folders is an identity-less account, if you only have
+        //local folders, it will be false.
+        //wizardCallback is true only when verifyAccounts is called from compose window or
+        //initial startup.
+        //the last condition in the if is so that we call account wizard only when the user
+        //has only a local folder and tries to compose mail.
+
+        if (openWizard || prefillAccount || ((!gAnyValidIdentity) && wizardCallback)) {
+            MsgAccountWizard(wizardCallback);
+            ret = false;
+        }
+        else
+        {
+          var localFoldersExists;
+          try
+          {
+            localFoldersExists = am.localFoldersServer;
+          }
+          catch (ex)
+          {
+            localFoldersExists = false;
+          }
+
+          // we didn't create the MsgAccountWizard - we need to verify that local folders exists.
+          if (!localFoldersExists)
+            am.createLocalMailAccount();
+        }
+
+        // This will do nothing on platforms without a shell service
+        const NS_SHELLSERVICE_CID = "@mozilla.org/suite/shell-service;1"
+        if (NS_SHELLSERVICE_CID in Components.classes)
+        {
+          // hack, set a time out to do this, so that the window can load first
+          setTimeout(showMailIntegrationDialog, 0);
+        }
+        return ret;
+    }
+    catch (ex) {
+        dump("error verifying accounts " + ex + "\n");
+        return false;
+    }
+}
+
+// we do this from a timer because if this is called from the onload=
+// handler, then the parent window doesn't appear until after the wizard
+// has closed, and this is confusing to the user
+function MsgAccountWizard(wizardCallback)
+{
+  setTimeout(function() { msgOpenAccountWizard(wizardCallback); }, 0);
+}
+
+function msgOpenAccountWizard(wizardCallback)
+{
+  gNewAccountToLoad = null;
+
+  window.openDialog("chrome://messenger/content/AccountWizard.xul", "AccountWizard",
+                    "chrome,modal,titlebar,centerscreen", {okCallback: wizardCallback});
+
+  loadInboxForNewAccount();
+
+  // For the first account we need to reset the default smtp server in the
+  // panel, by accessing smtpServers we are actually ensuring the smtp server
+  // list is loaded.
+  var smtpService = Components.classes["@mozilla.org/messengercompose/smtp;1"].getService(Components.interfaces.nsISmtpService);
+  var servers = smtpService.smtpServers;
+  try{ReloadSmtpPanel();}
+  catch(ex){}
+}
+
+// selectPage: the xul file name for the viewing page, 
+// null for the account main page, other pages are
+// 'am-server.xul', 'am-copies.xul', 'am-offline.xul', 
+// 'am-addressing.xul', 'am-smtp.xul'
+function MsgAccountManager(selectPage)
+{
+    var windowManager = Components.classes['@mozilla.org/appshell/window-mediator;1'].
+                            getService(Components.interfaces.nsIWindowMediator);
+
+    var existingAccountManager = windowManager.getMostRecentWindow("mailnews:accountmanager");
+
+    if (existingAccountManager)
+        existingAccountManager.focus();
+    else {
+        var server;
+        try {
+            var folderURI = GetSelectedFolderURI();
+            server = GetServer(folderURI);
+        } catch (ex) { /* functions might not be defined */}
+        
+        window.openDialog("chrome://messenger/content/AccountManager.xul",
+                          "AccountManager", "chrome,centerscreen,modal,titlebar",
+                          { server: server, selectPage: selectPage });
+    }
+}
+
+function loadInboxForNewAccount() 
+{
+  // gNewAccountToLoad is set in the final screen of the Account Wizard if a POP account
+  // was created, the download messages box is checked, and the wizard was opened from the 3pane
+  if (gNewAccountToLoad) {
+    var rootMsgFolder = gNewAccountToLoad.incomingServer.rootMsgFolder;
+    var inboxFolder = rootMsgFolder.getFolderWithFlags(0x1000);
+    SelectFolder(inboxFolder.URI);
+    window.focus();
+    setTimeout(MsgGetMessage, 0);
+    gNewAccountToLoad = null;
+  }
+}
+
+// returns true if we migrated - it knows this because 4.x did not have the
+// pref mailnews.quotingPrefs.version, so if it's not set, we're either 
+// migrating from 4.x, or a much older version of Mozilla.
+function migrateGlobalQuotingPrefs(allIdentities)
+{
+  // if reply_on_top and auto_quote exist then, if non-default
+  // migrate and delete, if default just delete.  
+  var reply_on_top = 0;
+  var auto_quote = true;
+  var quotingPrefs = 0;
+  var migrated = false;
+  try {
+    var prefService = Components.classes["@mozilla.org/preferences-service;1"]
+                                .getService(Components.interfaces.nsIPrefService);
+    var pref = prefService.getBranch(null);
+    quotingPrefs = pref.getIntPref("mailnews.quotingPrefs.version");
+  } catch (ex) {}
+  
+  // If the quotingPrefs version is 0 then we need to migrate our preferences
+  if (quotingPrefs == 0) {
+    migrated = true;
+    try {
+      reply_on_top = pref.getIntPref("mailnews.reply_on_top");
+      auto_quote = pref.getBoolPref("mail.auto_quote");
+    } catch (ex) {}
+
+    if (!auto_quote || reply_on_top) {
+      var numIdentities = allIdentities.Count();
+      var identity = null;
+      for (var j = 0; j < numIdentities; j++) {
+        identity = allIdentities.QueryElementAt(j, Components.interfaces.nsIMsgIdentity);
+        if (identity.valid) {
+          identity.autoQuote = auto_quote;
+          identity.replyOnTop = reply_on_top;
+        }
+      }
+    }
+    pref.setIntPref("mailnews.quotingPrefs.version", 1);
+  }
+  return migrated;
+}
